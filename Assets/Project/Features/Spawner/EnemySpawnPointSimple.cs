e4362a85ced5f6152435a14389926755e155b6d6
Kirill Sudarikov <sud-kir@yandex.ru>
2026-02-01 12:05:09 +0300

    wip: InspectorGUI SpawnPoint

--- Assets/Project/Features/Spawner/EnemySpawnPointSimple.cs
+++ Assets/Project/Features/Spawner/EnemySpawnPointSimple.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawnPointSimple : MonoBehaviour
@@ -6,56 +7,107 @@ public class EnemySpawnPointSimple : MonoBehaviour
     [Header("Enemy")]
     [SerializeField] GameObject enemyPrefab;
 
-
     [Header("Spawn")]
     [SerializeField] bool spawnOnStart = true;
     [SerializeField] float respawnDelay = -1f; // -1 = не респавнить
     [SerializeField] GameObject spawnFX;
 
-    GameObject currentEnemy;
+    private List<GameObject> spawnedEnemies = new List<GameObject>();
+    private bool spawnerEnabled = true;
+    private Coroutine respawnCoroutine;
 
     void Start()
     {
-        if (spawnOnStart)
+        if (spawnOnStart && spawnerEnabled)
             Spawn();
     }
 
-    void Spawn()
+    public void Spawn()
     {
-        if (currentEnemy != null)
-            return;
+        if (!spawnerEnabled) return;
 
         if (spawnFX != null)
             Instantiate(spawnFX, transform.position, Quaternion.identity);
 
-        currentEnemy = Instantiate(enemyPrefab, transform.position, transform.rotation);
+        GameObject newEnemy = Instantiate(enemyPrefab, transform.position, transform.rotation);
+        spawnedEnemies.Add(newEnemy);
 
-
-        // подписка на смерть
-        var health = currentEnemy.GetComponent<EnemyHealth>();
+        var health = newEnemy.GetComponent<EnemyHealth>();
         if (health != null)
-            health.OnDeath += OnEnemyDeath;
+            health.OnDeath += () => OnEnemyDeath(newEnemy);
     }
 
-    void OnEnemyDeath()
+    void OnEnemyDeath(GameObject enemy)
     {
-        if (currentEnemy == null) return;
+        if (enemy != null)
+        {
+            var health = enemy.GetComponent<EnemyHealth>();
+            if (health != null)
+                health.OnDeath -= () => OnEnemyDeath(enemy);
 
-        var health = currentEnemy.GetComponent<EnemyHealth>();
-        if (health != null)
-            health.OnDeath -= OnEnemyDeath;
+            spawnedEnemies.Remove(enemy);
+        }
 
-        //Destroy(currentEnemy);
-        currentEnemy = null;
-
-        if (respawnDelay > 0)
-            StartCoroutine(CoSpawnDelay());
+        if (spawnerEnabled && respawnDelay > 0)
+        {
+            respawnCoroutine = StartCoroutine(CoSpawnDelay());
+        }
     }
 
     IEnumerator CoSpawnDelay()
     {
         yield return new WaitForSeconds(respawnDelay);
         Spawn();
+    }
+
+    public void SetRespawnDelay(float delay)
+    {
+        respawnDelay = delay;
+    }
+
+    public void EnableSpawner()
+    {
+        spawnerEnabled = true;
+        if (spawnedEnemies.Count == 0 && respawnDelay > 0)
+        {
+            if (respawnCoroutine != null)
+                StopCoroutine(respawnCoroutine);
+            respawnCoroutine = StartCoroutine(CoSpawnDelay());
+        }
+    }
+
+    public void DisableSpawner()
+    {
+        spawnerEnabled = false;
+        if (respawnCoroutine != null)
+        {
+            StopCoroutine(respawnCoroutine);
+            respawnCoroutine = null;
+        }
+    }
+
+    public void ClearAllSpawnedEnemies()
+    {
+        foreach (var enemy in spawnedEnemies)
+        {
+            if (enemy != null)
+            {
+                var health = enemy.GetComponent<EnemyHealth>();
+                if (health != null)
+                    health.OnDeath -= () => OnEnemyDeath(enemy);
+
+                DestroyImmediate(enemy);
+            }
+        }
+        spawnedEnemies.Clear();
+
+        if (respawnCoroutine != null)
+        {
+            StopCoroutine(respawnCoroutine);
+            respawnCoroutine = null;
+        }
+
+        Debug.Log("Spawner cleared all spawned enemies (Editor).");
     }
 
     void OnDrawGizmos()
---
+++ Assets/Project/Features/Spawner/EnemySpawnPointSimpleEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EnemySpawnPointSimple))]
+public class EnemySpawnPointSimpleEditor : Editor
+{
+    
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        EnemySpawnPointSimple spawner = (EnemySpawnPointSimple)target;
+
+        GUILayout.Space(10);
+        GUILayout.Label("Debug Controls", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Spawn Enemy"))
+        {
+            spawner.Spawn();
+        }
+
+        if (GUILayout.Button("Enable Spawner"))
+        {
+            spawner.EnableSpawner();
+        }
+
+        if (GUILayout.Button("Disable Spawner"))
+        {
+            spawner.DisableSpawner();
+        }
+
+        if (GUILayout.Button("Clear All Spawned Enemies"))
+        {
+            spawner.ClearAllSpawnedEnemies();
+        }
+    }
+}
