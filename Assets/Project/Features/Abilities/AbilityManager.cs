c283bebfbc33be30e4442fcabcffe376e7b61bf2
DESKTOP-U7NGGN5\Valery <Valery>
2026-02-01 19:49:12 +0300

    fix: scene

--- Assets/Project/Features/Abilities/AbilityManager.cs
+++ Assets/Project/Features/Abilities/AbilityManager.cs
@@ -9,40 +9,40 @@ namespace Project.Features.Abilities
         [Header("Potions")]
         [SerializeField] private Vessel leftVessel;
         [SerializeField] private Vessel rightVessel;
-        
-        [SerializeField] private InputAction leftGulp;   
-        [SerializeField] private InputAction rightGulp;  
-    
-        [SerializeField] private float drinkWindow = .5f;      
-    
+
+        [SerializeField] private InputAction leftGulp;
+        [SerializeField] private InputAction rightGulp;
+
+        [SerializeField] private float drinkWindow = .5f;
+
         [SerializeField] private AbilityMixRatio[] abilities;
-    
+
         [SerializeField] private int maxTotalGulps = 3;
         public UnityEvent onOverload;
-    
+
         private int leftCount = 0;
         private int rightCount = 0;
         private float drinkingSartTime;
         private bool isDrinkingWindowActive = false;
-    
+
         private void OnEnable()
         {
             leftGulp?.Enable();
             rightGulp?.Enable();
-        
+
             leftGulp.performed += OnLeftGulp;
             rightGulp.performed += OnRightGulp;
         }
-    
+
         private void OnDisable()
         {
             leftGulp.performed -= OnLeftGulp;
             rightGulp.performed -= OnRightGulp;
-        
+
             leftGulp?.Disable();
             rightGulp?.Disable();
         }
-    
+
         private void Update()
         {
             if (isDrinkingWindowActive && Time.time > drinkingSartTime + drinkWindow)
@@ -50,37 +50,37 @@ namespace Project.Features.Abilities
                 ProcessMix();
             }
         }
-        
+
         private void OnLeftGulp(InputAction.CallbackContext context)
         {
-            if (leftVessel.dropped) return;
+            if (leftVessel.Dropped) return;
             if (!leftVessel.CanDrink)
             {
                 Debug.LogWarning("Left vessel is empty! Dropping...");
                 leftVessel.EmptyAndDrop();
                 return;
             }
-            
+
             leftVessel.Sip();
             leftCount++;
             StartDrinkingWindow();
         }
-    
+
         private void OnRightGulp(InputAction.CallbackContext context)
         {
-            if (rightVessel.dropped) return;
+            if (rightVessel.Dropped) return;
             if (!rightVessel.CanDrink)
             {
                 Debug.LogWarning("Right vessel is empty! Dropping...");
                 rightVessel.EmptyAndDrop();
                 return;
             }
-            
+
             rightVessel.Sip();
             rightCount++;
             StartDrinkingWindow();
         }
-    
+
         private void StartDrinkingWindow()
         {
             if (!isDrinkingWindowActive)
@@ -89,13 +89,13 @@ namespace Project.Features.Abilities
                 drinkingSartTime = Time.time;
             }
         }
-    
+
         private void ProcessMix()
         {
             int totalSwings = leftCount + rightCount;
-        
+
             Debug.Log($"Mix complete: L={leftCount}, R={rightCount}, Total={totalSwings}");
-        
+
             if (totalSwings > maxTotalGulps)
             {
                 Debug.LogWarning("💥 OVERLOAD!");
@@ -103,7 +103,7 @@ namespace Project.Features.Abilities
                 ResetMix();
                 return;
             }
-        
+
             foreach (var config in abilities)
             {
                 if (config.leftCount == leftCount && config.rightCount == rightCount)
@@ -112,23 +112,23 @@ namespace Project.Features.Abilities
                     break;
                 }
             }
-        
+
             ResetMix();
         }
-    
+
         private void ResetMix()
         {
             leftCount = 0;
             rightCount = 0;
             isDrinkingWindowActive = false;
         }
-    
+
         [System.Serializable]
         public class AbilityMixRatio
         {
             public int leftCount;
             public int rightCount;
-            [SerializeReference] public Ability ability; 
+            [SerializeReference] public Ability ability;
         }
     }
 }
--- Assets/Project/Features/Abilities/Vessel.cs
+++ Assets/Project/Features/Abilities/Vessel.cs
@@ -6,20 +6,20 @@ namespace Project.Features.Abilities
     {
         [SerializeField] private int maxCapacity = 3;
         public int currentAmount = 3;
-        
-        public bool dropped;
-        public bool CanDrink => currentAmount > 0; 
-        
+
+        public bool Dropped;
+        public bool CanDrink => currentAmount > 0;
+
         public void Sip()
         {
             if (!CanDrink) return;
             currentAmount--;
         }
-        
+
         public void EmptyAndDrop()
         {
             currentAmount = 0;
-            dropped = true;
+            Dropped = true;
         }
     }
 }
